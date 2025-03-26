10e8f398e20e05724d1bd68211f38317eb201894
hamsanguk <gtd5309@gmail.com>
2025-03-26 19:54:33 +0900

    day36

--- src/til/Day12.tsx
+++ src/til/Day12.tsx
@@ -1,24 +1,27 @@
 import React,{JSX} from "react";
-import "../css/dayform.css"
-const Day29=()=>{
-    return(
-        <dl>
-            <dt className="daybutton">3/14 day13</dt>
-            <dd>
-                <h3 className="daycontent">DApp 지갑</h3>
-                <ul>
-                    <li><h3>비결정적 월렛</h3></li>
-                    <li>각 키가 독립적으로 생성되며, 시드없이 무작위로 생성이된다,재사용 없음</li>
-                    <li>결정적(시드) 월렛</li>
-                    <li>하나의 시드에서 모든 비밀키를 파생, 각 비밀키들은 특정 인덱스와의 조합으로 결정)</li>
-                </ul>
-                <p>
-                    <h4>HD 월렛</h4>
-                    <p>
-                        
-                    </p>
-                </p>
-            </dd>
-        </dl>
-    )
-}
+import "../css/dayform.css";
+
+const Day29 = (): JSX.Element => {
+  return (
+    <dl>
+      <dt className="daybutton">3/14 Day 13</dt>
+      <dd>
+        <h3 className="daycontent">DApp 지갑</h3>
+        <ul>
+          <li>
+            <h3>비결정적 월렛</h3>
+          </li>
+          <li>각 키가 독립적으로 생성되며, 시드 없이 무작위로 생성되고 재사용되지 않음</li>
+          <li>
+            <h3>결정적(시드) 월렛</h3>
+          </li>
+          <li>하나의 시드에서 모든 비밀키를 파생, 각 비밀키들은 특정 인덱스와의 조합으로 결정</li>
+        </ul>
+        <h4>HD 월렛</h4>
+        <p>HD 월렛(계층적 결정적 월렛)은 BIP-32, BIP-39, BIP-44 표준을 따르며, 하나의 시드 문구에서 여러 개의 키를 파생할 수 있어 백업과 복구가 용이하다.</p>
+      </dd>
+    </dl>
+  );
+};
+
+export default Day29;
