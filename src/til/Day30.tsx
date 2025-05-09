cea72f73bc4b9ae51200d248c3ccd3ff242f6787
hamsanguk <gtd5309@gmail.com>
2025-05-09 17:32:29 +0900

    ExpressServer blog

--- src/til/Day30.tsx
+++ src/til/Day30.tsx
@@ -1,10 +1,16 @@
 import React,{JSX} from "react";
 
 const Day30 = ():JSX.Element=>{
+    
     return(
         <div>
         user  relayer가스비 대납 excute()  fowarder (nonces 소유)  recipient
         forwarder사용은 erc2771을 상속받지 않으면 평범한 erc20
+        <p>
+        solidity의 msg.sender:현 컨트랙트 기준에서 트랜잭션을 발생시킨 주소(eoa,컨트랙트의 주소)가 msg.sender
+        erc-20에서 스마트컨트랙트에서 msg.sender는 토큰의 소유권을 증명하고, 트랜잭션은 실행하는 중요한역할
+        transfer에서 사용:msg.sender가 자신의 토큰을 다른 주소로 전송할때
+        </p>
         </div>
     )
 }
--- src/til/Day31.tsx
+++ src/til/Day31.tsx
@@ -16,6 +16,7 @@ const Day31 = ():JSX.Element=>{
                                 proxy contract:중계자 역할, 사용자가 직접 사용할 외부 인터페이스, 사용자의 호출을 받아서 실제 구현된 implemantation Contract로 위임을 한다
                                 자신의 상태변수(storage)를 유지한채, 로직만 외부 컨트랙트에 위임
                                 핵심기능: delegatecall로 로직을 실행, implamentation주소를 저장한다, 업그레이드시 이 주소만 변경하면 다른 로직실행 가능
+                                상속받는 contract 각각의 모양을 명시
                             </li>
                             <li>
                                 implemantation contract:실제 비즈니스 로직을 실행,
@@ -23,14 +24,41 @@ const Day31 = ():JSX.Element=>{
                                 필요한 경우, 새로운 로직 버전을 만들어 proxy에 연결시켜 업그레이드 가능
                             </li>
                          </ul>
+                         <h4>user - upgradable proxycontract - implementation contract</h4>
                          <p>
-                            상태변수:evm의 영구저장공간 storage에 저당되는 변수, solidity에서는 상태변수선언을 순서대로 slot에 저장
-                            업그레이드 컨트랙트에서는 로직컨트랙트를 교체해도 storage는 proxy쪽에만 남겨야하므로 슬롯충돌 주의
+                            storage:CA와 연결된 고유함 저장공간,값은 트랜잭션 이후에도 계속 유지,가스비용이 높다(쓰기비용)
+                            저장방식- slot단위로 저장:순서는 상태변수 선언순서대로 슬롯 할당
+                            upgradable contract에서는 로직 컨트랙트를 교체해도 storage는 proxy쪽에만 남겨야 하여,
+                            슬롯 충돌을 주의해야 된다
+                         </p>
+                         <h3>eip1967슬롯고정방식</h3>
+                         <p>
+                            byte32 constant IMPLEMENTATION_SLOT = keccak256("eip1967.proxy.implementation")-1
+                            이 슬롯은 implementation address를 저장하기 위한 고유슬롯, 다른 상태변수와 절대 슬롯 충돌이 발생하지 않도록 고정된 위치에서 사용
                          </p>
                          <p>
-                            EIP-1967 슬롯고정방식
+                            업그레이더블컨트랙트에서 fallback함수와 delegatecall
+                            fallback함수-사용자 진입점
+                            delegatecall이 없다면 업그레이더블 패턴은 불가능하다
+                            사용자가 Proxy컨트랙트에 정의되지 않은 함수를 호출하면, 이 fallback함수가 실행되고,
+                            내부에서 delegatecall을 통해 실제 로직 컨트랙트로 요청을 넘긴다
                          </p>
-
+                         <p>
+                            proxyContract  -delegatecall() -  logic(implementation)contract
+                            proxy컨트랙트에서 다른 컨트랙트(implementation)의 코드를 실행시킨다
+                            단,proxy의 상태를 그대로 사용중, 즉 로직은 implementation이 제공하며 저장역할은 proxy가 담당
+                         </p>
+                         <h4>유저가 프록시에 호출 - fallback 작동 - delegatecall실행 - 로직 컨트랙트의 코드 실행
+                            - 결과는 proxy의 storage에 저장
+                         </h4>
+                         <p>
+                            <h3>proxy pattern을 위한 구성요소3가지</h3>
+                            <ul>
+                                <li>proxy contract:유저가 호출하는 진입점.실제 로직이 없으며 delegatecall을 이용하여 위임</li>
+                                <li>Implementation contract(logic):실제 기능이 정의된 컨트랙트</li>
+                                <li>Storage(proxy 내부):데이터는 proxy에 저장된다(delegatecall)</li>
+                            </ul>
+                         </p>
                     </p>
                 </dd>
             </dl>
